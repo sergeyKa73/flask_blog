a76683d40d1dfd7847b6f2201164711ca77b6acf
Sergey <karandeev73@gmail.com>
2021-10-11 20:21:19 +0300

    feat: add basic page

--- app/view.py
+++ app/view.py
@@ -1,7 +1,8 @@
 from app import app
-
+from flask import render_template
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    name = 'Ivan'
+    return render_template('index.html', n = name)
     
